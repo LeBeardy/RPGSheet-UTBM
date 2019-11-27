78562f022c85dad9df9cd0218617bfb9232d6902
Mathieu <Mathieu@DESKTOP-354848G>
2019-11-27 12:07:59 +0100

    PDFExport

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -3,6 +3,8 @@ import {SelectComponent} from '../models/selectComponent';
 import {Rule} from '../models/rule';
 import {Serialize, serialize} from 'cerialize';
 import {Canvas} from '../models/canvas';
+import * as jspdf from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-tab2',
@@ -46,11 +48,25 @@ export class Tab2Page {
       alert(JSON.stringify(Serialize(this.canvas)));
     }
 
-    ExportPDFCanvas(){
-      alert('pdf');
-    }
-  toggleTooltip(): void {
-    this.tooltipExpand = !this.tooltipExpand;
+    ExportPDFCanvas() {
+      const data = document.getElementById('canvas');
+      html2canvas(data).then(canvas => {
+        const imgWidth = 208;
+        const pageHeight = 295;
+        const imgHeight = canvas.height * imgWidth / canvas.width;
+        const heightLeft = imgHeight;
+
+        const contentDataURL = canvas.toDataURL('image/png')
+        const pdf = new jspdf('p', 'mm', 'a4');
+        const position = 0;
+        pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)
+        pdf.save('MYPdf.pdf');
+
+      });
+      }
+
+    toggleTooltip(): void {
+     this.tooltipExpand = !this.tooltipExpand;
   }
 
 }
